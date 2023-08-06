7ce0b60f2c3f3843d4081ca8c1a3ca77ec3d9d22
neltia <dsz08082@naver.com>
2023-08-06 11:05:01 +0000

    fix: 포트 변경

--- app/run_debug.sh
+++ app/run_debug.sh
@@ -1,3 +1,3 @@
 export DJANGO_SETTINGS_MODULE=config.settings.debug
 echo $DJANGO_SETTINGS_MODULE
-python manage.py runserver 0.0.0.0:8080 --settings=config.settings.debug
+python manage.py runserver 0.0.0.0:8000 --settings=config.settings.debug
